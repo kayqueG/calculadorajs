dfbe43cef95cbb855dcd5c4d89c67caed49d92f2
kayque <kayquegps2@gmail.com>
2021-11-21 20:20:26 -0300

    metodos para apagar com backspace, adicionar virgula e pegar os digitos do usuario atraves do teclado finalizados!

--- calculadora.js
+++ calculadora.js
@@ -13,7 +13,7 @@ const operacaoPendente = () => operador != undefined;
 const calcular = () => {
     if (operacaoPendente()) {
         novoNumero = true
-        const numeroAtual = parseFloat(display.textContent);
+        const numeroAtual = parseFloat(display.textContent.replace(',', '.'));  // troca a virgula pelo ponto
         if (operador == '+') {
             atualizarDisplay(numeroAnterior + numeroAtual);
         } else if (operador == '-') {
@@ -28,10 +28,10 @@ const calcular = () => {
 
 const atualizarDisplay = (texto) => {
     if (novoNumero) {
-        display.textContent = texto;
+        display.textContent = texto.toLocaleString('BR'); // sempre que atualizar o display, troca de ponto para virgula
         novoNumero = false;
     } else {
-        display.textContent += texto;
+        display.textContent += texto.toLocaleString('BR');
     }
 
 
@@ -44,7 +44,7 @@ const selecionarOperador = (evento) => {
         calcular();
         novoNumero = true;
         operador = evento.target.textContent;
-        numeroAnterior = parseFloat(display.textContent);
+        numeroAnterior = parseFloat(display.textContent.replace(',', '.'));
     }
 }
 
@@ -74,3 +74,69 @@ const limparCalculo = () => {
 }
 
 document.getElementById('limparCalculo').addEventListener('click', limparCalculo);
+
+const removerUltimoCaracter = () => display.textContent = display.textContent.slice(0, -1); //remove o ultimp
+
+document.getElementById('backspace').addEventListener('click', removerUltimoCaracter);
+
+const inverterOperador = () => {
+    novoNumero = true // atualiza o display com o numero
+    atualizarDisplay(display.textContent * -1);
+}
+
+
+document.getElementById('inverter').addEventListener('click', inverterOperador);
+
+const existeVirgula = () => display.textContent.indexOf(',') != -1;
+
+
+const existeValor = () => display.textContent.length > 0;
+
+
+const inserirVirgula = () => {
+    if (!existeVirgula()) {
+        if (existeValor()) {
+            atualizarDisplay(',');
+        } else {
+            atualizarDisplay('0,');
+        }
+    }
+}
+document.getElementById('decimal').addEventListener('click', inserirVirgula);
+
+// objeto para pegar as teclas digitadas pelo usuario
+const teclado = {
+    '0': 'tecla0',
+    '1': 'tecla1',
+    '2': 'tecla2',
+    '3': 'tecla3',
+    '4': 'tecla4',
+    '5': 'tecla5',
+    '6': 'tecla6',
+    '7': 'tecla7',
+    '8': 'tecla8',
+    '9': 'tecla9',
+    'Backspace': 'backspace',
+    'Escape': 'limparCalculo',
+    '/': 'operadorDivisao',
+    '*': 'operadorMultiplicacao',
+    '+': 'operadorAdicao',
+    '-': 'operadorSubtracao',
+    ',': 'decimal',
+    'c': 'limparDisplay',
+    '=': 'igual',
+}
+
+const mapearTeclado = (evento) => {
+    const tecla = evento.key;
+
+    const teclaPermitida = () => Object.keys(teclado).indexOf(tecla) != -1 // verificando se existe a tecla no objeto teclado a partir de suas chaves
+
+
+    if (teclaPermitida()) {
+        document.getElementById(teclado[tecla]).click();
+    }
+}
+document.addEventListener('keydown', mapearTeclado);
+
+
